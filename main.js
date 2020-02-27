b39a242d5ac0888b0aff54776307945943465569
kjdowns <kevin.downs04@gmail.com>
2020-02-27 14:28:20 -0500

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.getElementsByClassName('like-glyph');
+const errorModal = document.getElementById('modal');
 
+function addEventToHeart() {
+  for(heart of hearts){
+    heart.addEventListener('click', function(e){
+      mimicServerCall()
+      .then(test1 =>{
+        console.log(test1)
+        toggleHeart(e.target);
+      })
+      .catch(message => {
+        errorModal.classList.remove('hidden');
+        errorModal.children[1].innerText = message;
+        setTimeout(function(){
+          errorModal.classList.add('hidden');
+        }, 5000);
+      })
+    })
+  };
+};
 
+function toggleHeart(heart) {
+  if (heart.innerText === EMPTY_HEART) {
+    heart.innerText = FULL_HEART;
+    heart.classList.add('activated-heart');
+  } else {
+    heart.innerText = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  };
+};
 
+window.addEventListener('DOMContentLoaded', function(e){
+  addEventToHeart();
+});
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
